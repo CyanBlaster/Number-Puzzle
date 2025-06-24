7ab753ff4b6224fa3d9af007b7d207ca72b072f3
CyanBlaster <michael.ng.do@gmail.com>
2025-06-23 22:25:57 -0400

    Built the initial board for the Number Puzzle!

---
+++ Number_Puzzle.py
@@ -0,0 +1,74 @@
+import sys
+import pygame
+import datetime
+import math
+import pygame.freetype
+
+pygame.init()
+GAME_FONT = pygame.freetype.Font("DayDream.ttf", 24)
+
+
+def ZeroField(n):
+    return [[0] * n for i in range(n)]
+def main():
+    array = [[0, 1, 2, 3, 4], [6, 5, 7, 8, 9], [10, 11, 12, 13, 14], [15, 16, 17, 18, 19], [20, 21, 22, 23, 24]]
+    width = 501
+    height = 501
+    screen = pygame.display.set_mode((width, height))
+    running = True
+    xIdx = 2
+    yIdx = 2
+    turn = 1
+    board = ZeroField(20)
+    while running:
+        pygame.display.flip()
+        screen.fill((0, 0, 0))
+        for i in range(0, 5):
+            pygame.draw.line(screen, (0, 0, 255), (i * 500/5, 0), (i * 500/5, 500))
+            pygame.draw.line(screen, (0, 0, 255), (0, i * 500/5), (500, i * 500/5))
+        pygame.draw.rect(screen, (255, 255, 255), (xIdx * 100 + 1, yIdx * 100 + 1, 99, 99))
+
+        for x in range(5):
+            for y in range(5):
+                if(array[x][y] != 0):
+                    text_surface, rect = GAME_FONT.render(str(array[x][y]), (0, 0, 0))
+                    screen.blit(text_surface, (y * 100 + 50 - rect.width/2, x * 100 + 50 - rect.height/2))
+
+
+
+
+
+
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                running = False
+            elif events.type == pygame.KEYDOWN:
+                if events.key == pygame.K_LEFT:
+                    xIdx -= 1
+                    if(xIdx == -1):
+                        xIdx = 0
+                elif events.key == pygame.K_RIGHT:
+                    xIdx += 1
+                    if(xIdx == 5):
+                        xIdx = 4
+                elif events.key == pygame.K_UP:
+                    yIdx -= 1
+                    if(yIdx == -1):
+                        yIdx = 0
+                elif events.key == pygame.K_DOWN:
+                    yIdx += 1
+                    if(yIdx == 5):
+                        yIdx = 4
+                elif events.key == pygame.K_SPACE:
+                    if(board[xIdx][yIdx] == 0):
+                        if(turn == 1):
+                            print(1)
+                            board[xIdx][yIdx] = 1
+                            turn = 2
+                        else:
+                            print(2)
+                            board[xIdx][yIdx] = 2
+                            turn = 1
+                    
+
+main()
